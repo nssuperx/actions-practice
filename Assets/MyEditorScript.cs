cd9ede6874445c761748666427f26e4d0ac531ac
twugo <hm17031@student.miyazaki-u.ac.jp>
2021-10-30 21:23:54 +0900

    Update: build Sceneを読み込むようにした

--- Assets/MyEditorScript.cs
+++ Assets/MyEditorScript.cs
@@ -1,10 +1,20 @@
+using System.Collections.Generic;
 using UnityEditor;
 class MyEditorScript
 {
     static void PerformBuild()
     {
+        List<string> scenePaths = new List<string>();
+        foreach (EditorBuildSettingsScene editorBuildSettingsScene in EditorBuildSettings.scenes)
+        {
+            if (editorBuildSettingsScene.enabled == true)
+            {
+                scenePaths.Add(editorBuildSettingsScene.path);
+            }
+        }
+
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = new[] { "Assets/Scene/SampleScene.unity" };
+        buildPlayerOptions.scenes = scenePaths.ToArray();
         buildPlayerOptions.locationPathName = "yourLocationNameHere";
         buildPlayerOptions.target = BuildTarget.WebGL;
         buildPlayerOptions.options = BuildOptions.None; // set whatever you want here
